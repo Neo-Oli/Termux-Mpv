acfda8823639696cd86f65b41c1d36599ad5cba8
Alex Fence <alexfence.code@gmail.com>
2019-08-02 01:51:56 +0200

    mostly fix code style (PEP8)

--- termuxmpv/__main__.py
+++ termuxmpv/__main__.py
@@ -8,52 +8,65 @@ import time
 import tempfile
 import json
 import select
-class termuxmpv:
-    def signal_handler(self,signal, frame):
+
+
+class Termuxmpv:
+    def signal_handler(self, signal, frame):
         pass
-    def __init__(self,args):
+
+    def __init__(self, args):
         signal.signal(signal.SIGINT, self.signal_handler)
-        self.args=args
-        self.pause=False
-        self.q=[]
-        self.metadata={}
+        self.args = args
+        self.pause = False
+        self.q = []
+        self.metadata = {}
         self.initFifo()
         if not self.checkForSocket():
             self.createSocket()
         self.startProcess()
         self.getSocket()
-        self.first=True
+        self.first = True
         self.processMessage()
-        self.first=False
+        self.first = False
         self.updateNotification()
         self.monitor()
+
     def __del__(self):
         self.cleanup()
+
     def checkForSocket(self):
-        prev=""
+        prev = ""
         for arg in reversed(self.args):
-            if arg=="--input-ipc-server":
-                self.sockpath=prev
+            if arg == "--input-ipc-server":
+                self.sockpath = prev
                 return True
             elif arg.startswith("--input-ipc-server="):
-                self.sockpath=arg.split("=")[1]
+                self.sockpath = arg.split("=")[1]
                 return True
 
-            prev=arg
+            prev = arg
         return False
+
     def initFifo(self):
-        self.notificationId="termuxMpv.{}".format(time.time())
-        self.fifoname="/data/data/com.termux/files/usr/tmp/{}".format(self.notificationId)
+        self.notificationId = "termuxMpv.{}".format(time.time())
+        self.fifoname = "/data/data/com.termux/files/usr/tmp/{}".format(
+            self.notificationId)
         os.mkfifo(self.fifoname)
         self.fifo = os.open(self.fifoname, os.O_RDONLY | os.O_NONBLOCK)
+
     def startProcess(self):
-        prefix=os.environ["PREFIX"]
-        program="{}/bin/mpv".format(prefix)
-        self.mpvproc=subprocess.Popen([program, '--input-ipc-server', self.sockpath] + self.args ,stdin=sys.stdin) 
+        prefix = os.environ["PREFIX"]
+        program = "{}/bin/mpv".format(prefix)
+        self.mpvproc = subprocess.Popen(
+            [program, '--input-ipc-server', self.sockpath] + self.args,
+            stdin=sys.stdin
+        )
+
     def createSocket(self):
         fd, self.sockpath = tempfile.mkstemp(prefix="mpv.")
         os.close(fd)
         os.remove(self.sockpath)
+
     def getSocket(self):
         while self.isRunning():
             time.sleep(0.1)
@@ -64,11 +77,13 @@ class termuxmpv:
                 continue
             else:
                 break
+
     def isRunning(self):
         if self.mpvproc.poll() is None:
             return True
         else:
             sys.exit(self.mpvproc.returncode)
+
     def cleanup(self):
         try:
             os.close(self.fifo)
@@ -79,28 +94,30 @@ class termuxmpv:
         except FileNotFoundError:
             pass
         if self.notificationId:
-            command=["termux-notification-remove",self.notificationId]
-            output=subprocess.call(command)
+            command = ["termux-notification-remove", self.notificationId]
+            # TODO variable never used
+            output = subprocess.call(command)
         if self.sockpath:
             try:
                 os.remove(self.sockpath)
             except OSError:
                 pass
         self.updatehook()
+
     def monitor(self):
         while self.isRunning():
             # time.sleep(1)
-            b=True
+            b = True
             buf = b""
             while b:
-                b=None
+                b = None
                 r, w, e = select.select([self.sock], [], [], 1)
                 if r:
                     b = self.sock.recv(1024)
                 if not b:
                     break
                 buf += b
-            buf=buf.decode("utf-8",'replace')
+            buf = buf.decode("utf-8", 'replace')
             newline = buf.find("\n")
             while newline >= 0:
                 message = buf[:newline + 1]
@@ -108,33 +125,35 @@ class termuxmpv:
                 newline = buf.find("\n")
 
                 self.processMessage(message)
-            command=""
+            command = ""
             while True:
-                part=os.read(self.fifo,1024).decode("utf-8")
-                command="{}{}".format(command,part)
+                part = os.read(self.fifo, 1024).decode("utf-8")
+                command = "{}{}".format(command, part)
                 if not part:
                     break
             if command:
                 self.sendCommand(command)
-    def sendCommand(self,command):
-        command=command.strip()
-        if command=="prev":
-            self.sendMessage(["keypress","<"],"keypress")
-        if command=="next":
-            self.sendMessage(["keypress",">"],"keypress")
-        if command=="pause":
-            self.sendMessage(["keypress","p"],"keypress")
-        if command=="exit":
-            self.sendMessage(["keypress","q"],"keypress")
-        if command=="updateNotification":
+
+    def sendCommand(self, command):
+        command = command.strip()
+        if command == "prev":
+            self.sendMessage(["keypress", "<"], "keypress")
+        if command == "next":
+            self.sendMessage(["keypress", ">"], "keypress")
+        if command == "pause":
+            self.sendMessage(["keypress", "p"], "keypress")
+        if command == "exit":
+            self.sendMessage(["keypress", "q"], "keypress")
+        if command == "updateNotification":
             self.updateNotification()
-    def sendMessage(self,message,msgprocessor):
+
+    def sendMessage(self, message, msgprocessor):
         self.q.append(msgprocessor)
-        data={}
-        data["command"]=message
-        data=json.dumps(data)
-        data="{}\n".format(data)
-        data=data.encode("utf-8")
+        data = {}
+        data["command"] = message
+        data = json.dumps(data)
+        data = "{}\n".format(data)
+        data = data.encode("utf-8")
         while data:
             try:
                 size = self.sock.send(data)
@@ -147,97 +166,104 @@ class termuxmpv:
 
     def processMessage(self, message='{"event":""}'):
         try:
-            message=json.loads(message)
-        except:
+            message = json.loads(message)
+        except Exception:
             pass
         if "event" in message:
-            if message["event"]=="metadata-update" or self.first:
-                self.sendMessage(["get_property","metadata"],"metadata")
-            if message["event"]=="file-loaded" or self.first:
-                self.sendMessage(["get_property","filename"],"filename")
-            if message["event"]=="pause":
-                self.pause=True
+            if message["event"] == "metadata-update" or self.first:
+                self.sendMessage(["get_property", "metadata"], "metadata")
+            if message["event"] == "file-loaded" or self.first:
+                self.sendMessage(["get_property", "filename"], "filename")
+            if message["event"] == "pause":
+                self.pause = True
                 self.updateNotification()
-            if message["event"]=="unpause":
-                self.pause=False
+            if message["event"] == "unpause":
+                self.pause = False
                 self.updateNotification()
         elif "data" in message:
             if self.q[0]:
-                if self.q[0]=="metadata":
-                    self.metadata=message["data"]
+                if self.q[0] == "metadata":
+                    self.metadata = message["data"]
                     self.updateNotification()
-                if self.q[0]=="filename":
-                    self.filename=message["data"]
+                if self.q[0] == "filename":
+                    self.filename = message["data"]
                     self.updateNotification()
                 del self.q[0]
         elif "error" in message:
             del self.q[0]
+
     def updatehook(self):
-        command="hook-update-mpv"
-        devnull=open(os.devnull, 'wb')
+        command = "hook-update-mpv"
+        devnull = open(os.devnull, 'wb')
         try:
-            subprocess.call(["sh", "-c", command],stdout=devnull,stderr=devnull)
-        except:
+            subprocess.call(["sh", "-c", command],
+                            stdout=devnull, stderr=devnull)
+        except Exception:
             pass
+
     def updateNotification(self):
         self.updatehook()
         # padding="           "
-        #disable padding for now
-        padding=""
+        # disable padding for now
+        padding = ""
         # playbutton="{}⏸{}".format(padding,padding)
         # prevbutton="{}⏮{}".format(padding,padding)
         # nextbutton="{}⏭{}".format(padding,padding)
-        playbutton="{}❙❙{}".format(padding,padding)
-        prevbutton="{}|◀◀{}".format(padding,padding)
-        nextbutton="{}▶▶|{}".format(padding,padding)
+        playbutton = "{}❙❙{}".format(padding, padding)
+        prevbutton = "{}|◀◀{}".format(padding, padding)
+        nextbutton = "{}▶▶|{}".format(padding, padding)
         if self.pause:
-            playbutton="{} ▶ {}".format(padding,padding)
-        metadata={}
-        for attr in ["album","artist","title","icy-title"]:
+            playbutton = "{} ▶ {}".format(padding, padding)
+        metadata = {}
+        for attr in ["album", "artist", "title", "icy-title"]:
             try:
-                metadata[attr]=self.metadata[attr]
+                metadata[attr] = self.metadata[attr]
             except KeyError:
                 try:
-                    metadata[attr]=self.metadata[attr.upper()]
+                    metadata[attr] = self.metadata[attr.upper()]
                 except KeyError:
-                    metadata[attr]="None"
+                    metadata[attr] = "None"
         try:
-            filename=self.filename
+            filename = self.filename
         except AttributeError:
-            filename="None"
+            filename = "None"
 
-        if metadata["title"]!="None":
-            title=metadata["title"]
-        if metadata["icy-title"]!="None":
-            title=metadata["icy-title"]
+        if metadata["title"] != "None":
+            title = metadata["title"]
+        if metadata["icy-title"] != "None":
+            title = metadata["icy-title"]
         else:
-            title=filename
-        command=[
+            title = filename
+        command = [
             "termux-notification",
             "--id", self.notificationId,
             "--title", title,
-            "--content", "{}, {}".format(metadata["artist"], metadata["album"]),
+            "--content", "{}, {}".format(metadata["artist"],
+                                         metadata["album"]),
             # "--led-color 00ffff",
             # "--vibrate 300,150,300",
             "--priority", "max",
             "--action", ";".join([
-                "am start --user 0 -n com.termux/com.termux.app.TermuxActivity",
+                "am start --user 0 -n com.termux/com.termux.app.TermuxActivity", # noqa line break is unreasonable
                 "echo 'updateNotification'> {}".format(self.fifoname)
-                ]),
+            ]),
             "--button1", prevbutton,
-            "--button1-action","echo 'prev'> {}".format(self.fifoname),
+            "--button1-action", "echo 'prev'> {}".format(self.fifoname),
             "--button2", playbutton,
-            "--button2-action","echo 'pause'> {}".format(self.fifoname),
+            "--button2-action", "echo 'pause'> {}".format(self.fifoname),
             "--button3", nextbutton,
-            "--button3-action","echo 'next'> {}".format(self.fifoname),
+            "--button3-action", "echo 'next'> {}".format(self.fifoname),
             "--on-delete", "echo 'exit'>{}".format(self.fifoname),
         ]
-        output=subprocess.call(command)
+        # TODO variable never used
+        output = subprocess.call(command)
+
+
 def main(args=None):
     if args is None:
         args = sys.argv[1:]
-    termuxmpv(args)
+    Termuxmpv(args)
+
+
 if __name__ == "__main__":
     main()
-
-
